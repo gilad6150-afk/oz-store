bcc40c94635e220e2331f5b71d2079ee7737417b
gilad6150-afk <gilad6150@gmail.com>
2026-09-20 00:38:50 +0300

    Generate and deploy Google Merchant Center XML feed for 167 products

---
+++ build_google_merchant_feed.ps1
@@ -0,0 +1,62 @@
+$utf8NoBOM = New-Object System.Text.UTF8Encoding($false)
+$dir = "C:\Users\97254\.gemini\antigravity\scratch\oz-store"
+
+# Read products_data.js
+$productsJs = [System.IO.File]::ReadAllText((Join-Path $dir "products_data.js"), $utf8NoBOM)
+
+# Extract JSON array from window.PRODUCTS_DATA = [...]
+$jsonStart = $productsJs.IndexOf('[')
+$jsonEnd = $productsJs.LastIndexOf(']')
+$jsonStr = $productsJs.Substring($jsonStart, $jsonEnd - $jsonStart + 1)
+
+$products = $jsonStr | ConvertFrom-Json
+
+Write-Host "Loaded" $products.Count "products for Google Merchant Feed generator"
+
+$xml = @"
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
+  <channel>
+    <title>מכון עוז - Google Merchant Center Product Feed</title>
+    <link>https://oz-judaica.co.il</link>
+    <description>פיד מוצרים רשמי עבור Google Shopping והופעה במנוע החיפוש של גוגל</description>
+"@
+
+foreach ($p in $products) {
+    $id = $p.id
+    $sku = if ($p.sku) { $p.sku } else { "OZ-$id" }
+    $title = [System.Security.SecurityElement]::Escape($p.name)
+    $desc = [System.Security.SecurityElement]::Escape($p.short_description)
+    $link = "https://oz-judaica.co.il/#product-$id"
+    $img = $p.image
+    $price = "$($p.price) ILS"
+    $avail = if ($p.inStock) { "in_stock" } else { "out_of_stock" }
+    $catName = [System.Security.SecurityElement]::Escape($p.category_name)
+
+    $xml += @"
+
+    <item>
+      <g:id>$sku</g:id>
+      <title>$title</title>
+      <description>$desc</description>
+      <link>$link</link>
+      <g:image_link>$img</g:image_link>
+      <g:condition>new</g:condition>
+      <g:availability>$avail</g:availability>
+      <g:price>$price</g:price>
+      <g:brand>מכון עוז</g:brand>
+      <g:product_type>$catName</g:product_type>
+      <g:identifier_exists>no</g:identifier_exists>
+    </item>
+"@
+}
+
+$xml += @"
+
+  </channel>
+</rss>
+"@
+
+$feedPath = Join-Path $dir "google_merchant_feed.xml"
+[System.IO.File]::WriteAllText($feedPath, $xml, $utf8NoBOM)
+Write-Host "Google Merchant XML Feed generated successfully at google_merchant_feed.xml!"
